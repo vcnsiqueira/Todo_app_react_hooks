df8b290354736f8528986bea2847f3571e46d655
vcnsiqueira <vcnsiqueira@gmail.com>
2020-11-22 23:49:06 -0300

    I included the menu component based on some navbar from Bootstrap

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,10 +5,12 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Todo from './components/Todo/Todo';
 import About from './components/About/About';
+import Menu from './template/Menu';
 
 const App = () => {
   return (
     <div className='container'>
+      <Menu />
       <About />
       <Todo />
     </div>
---
+++ frontend/src/template/Menu.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const Menu = () => {
+    return(
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+            <a className="navbar-brand" href="#">
+                Todo App
+            </a>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+            </button>
+
+            <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+            <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
+                <li className="nav-item">
+                <a className="nav-link" href="#/todos">Tarefas</a>
+                </li>
+                <li className="nav-item">
+                <a className="nav-link" href="#/about">Sobre</a>
+                </li>
+            </ul>
+            <form className="form-inline my-2 my-lg-0">
+                <input className="form-control mr-sm-2" type="search" placeholder="Procurar"/>
+                <button className="btn btn-outline-primary my-2 my-sm-0" type="submit">Search</button>
+            </form>
+            </div>
+        </nav>
+    );
+};
+
+export default Menu;
